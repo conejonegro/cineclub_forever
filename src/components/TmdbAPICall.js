4ead7d0f9d2e62441aaa5ddc64e2bedb3a6bcdfd
Luis Rosales <lantoniorosales-contractor@dealerinspire.com>
2023-10-04 23:05:50 -0600

    gran avance al tmdb con ayuda de chatfpt

--- src/components/TmdbAPICall.js
+++ src/components/TmdbAPICall.js
@@ -1,61 +1,90 @@
 import axios from 'axios';
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import peliculasData from '../json/peliculasData';
-
+import React, { useState, useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const API_KEY = 'd35b24b361166e540ee6c082ddecd6bf';
-const movie_id = 552;
-const movie_ids = [550, 552];
+const movies_id = [{ 
+                      id: 550
+                    }, 
+                    { 
+                      id: 552
+                    },
+                    { 
+                      id: 788734
+                    },
+                    { 
+                      id: 660942
+                    }
+                  ];
+const IMG_PATH = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
 
-const peliculaNombre = peliculasData.map( (pelicula) => (
-   pelicula.nombre
-   
-
-));
-// console.log(peliculaNombre)
- const nameQuery = "donnie+darko";
+//  const nameQuery = "donnie+darko";
 // const baseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
- const baseURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`;
+//  const baseURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`;
 //  const baseURL = `https://api.themoviedb.org/3/movie/346698/keywords?api_key=${API_KEY}`;
 // const baseURL = `https://api.themoviedb.org/3/search/movie?query=${nameQuery}&api_key=${API_KEY}`;
 
+function TmdbApiCall() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-function TmdbApiCall(){
+  useEffect(() => {
 
-    const [post, setPost] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
-    
+    const fetchData = async () => {
 
-    React.useEffect(() => {
-        // console.log("entra use efect")
+      try {
+        const responses = await Promise.all(
+
+          movies_id.map((movie) =>
+            axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${API_KEY}`)
+
+          )
+        );
         
-        const movies_from_api = movie_ids.map( (movie_id) => {
+        const postData = responses.map((response) => (
+          response.data
+          ));
 
-          axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}`).then((response) => {
-            setPost(response.data);
-            setLoading(false);
-            // console.log(response.data)
-            
-          });
-
-        } )
- 
-      }, []);
-        
-      if (loading) {
-        return <p>Cargando...</p>;
-      }else{
-      
-        // const datos = Object.values(post);
-           console.log(post)
-        return (
-          <Container className='my-4'>
-            <h1>{post.original_title}</h1>
-          </Container>
-         );   
+        setPosts(postData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
       }
-     
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
+  console.log(posts)
+  return (
+   
+    <div id="peliculasCont" className='container'>
+        <h1>Todas las peliculas</h1>
+        <Row className="justify-content-between ">
+          <div className="main-container-pelicula col-md-6 col-xl-4 col-xxl-3" >
+
+
+      {posts.map((post) => (
+        <div key={post.id} className='movie-container'>
+          
+          <Link to={"/peliculas-detalle/"+post.original_title}>
+           
+            <img src={IMG_PATH+post.poster_path} alt={post.original_title} className="poster "/>
+            <h2>{post.original_title}</h2>
+            <p>{post.overview.substring(0, 80)+'...'}</p>
+          </Link>
+         
+        </div>
+      ))}
+      </div>
+      </Row>
+    </div>
+  );
 }
 
 export default TmdbApiCall;
