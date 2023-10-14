f1fb4ccd4d43346eac48eb69b50feeb3749a23b3
Luis Rosales <lantoniorosales-contractor@dealerinspire.com>
2023-10-14 15:13:41 -0600

    pelicula detalle comes now from api

--- src/pages/pelicula-detalle.js
+++ src/pages/pelicula-detalle.js
@@ -9,12 +9,13 @@ import axios from "axios";
 
 const API_KEY = 'd35b24b361166e540ee6c082ddecd6bf';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
-const movies_id = [{ id: 550  }, { id: 552 },{ id: 788734 },{ id: 660942 },{ id: 9426 },{ id: 780609  }];
+const movies_id = [{ id: 550  }, { id: 552 },{ id: 788734 },{ id: 660942 },{ id: 9426 },{ id: 780609  }, { id: 882598}];
 
 function PeliculaDetalle({}){
 // API CALL
 
 const [posts, setPosts] = useState([]);
+const [loading, setLoading] = useState(true);
 
   useEffect(() => {
 
@@ -34,6 +35,7 @@ const [posts, setPosts] = useState([]);
           ));
 
         setPosts(postData);
+        setLoading(false)
        
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -49,43 +51,48 @@ const [posts, setPosts] = useState([]);
   let myPosts = posts.map((post) => {
     post.title.toLowerCase();
     return(
-        [{original_title: post.original_title, slug: post.title.toLowerCase().replace(/\s+/g, '-'), id: post.id, poster: post.poster_path, sinopsis: post.overview, release_date: post.release_date}]
+        {original_title: post.original_title, slug: post.title.toLowerCase().replace(/\s+/g, '-'), id: post.id, poster: post.poster_path, sinopsis: post.overview, release_date: post.release_date}
     )});
 
     console.log(myPosts)
 
 
      const { slug } = useParams();
-     const peliculasDataLooped = posts.find((post) => (post.slug === slug));
-    //  console.log(peliculasDataLooped);
+     const peliculasDataLooped = myPosts.find((post) => (post.slug === slug));
+     console.log(peliculasDataLooped);
     //   console.log(myPosts);
     
+    
+
     return(
-        <h1>hola</h1>
-        // <section className="main-movie-banner" style={{backgroundImage: `url(${peliculasDataLooped.poster})`}}>
-        //     <div className="banner-support">
-        //         <Container className="container">
-        //             <Row>
-        //                 <div className="col-lg-4 poster">
-        //                     <img src={peliculasDataLooped.poster} alt="Poster descriptivo" />
-        //                 </div>
-        //                 <div className="col-lg-8 movie-info">
-        //                     <div className="titulo-pelicula">
-        //                         <h1>{peliculasDataLooped.title}</h1><span>({peliculasDataLooped.release_date})</span>
-        //                         {/* <p>{peliculasDataLooped.genero}</p> */}
-        //                     </div>
-        //                     <p>{peliculasDataLooped.sinopsis}</p> 
-        //                     {/* <p>{peliculasDataLooped.director}</p> */}
-        //                     <p><b>Fecha de Lanzamiento</b> {peliculasDataLooped.release_date}</p>
-        //                 </div>
-        //             </Row>   
-        //         </Container>
-        //     </div>
-        //     {/* <Video url={peliculasDataLooped.video_url} subtitles={peliculasDataLooped.subtitles} /> */}
-        // </section>
+      <>
+        {loading ? 'loading' : 
+          <section className="main-movie-banner" style={{backgroundImage: `url(${IMG_PATH+peliculasDataLooped.poster})`}}>
+            <div className="banner-support">
+                <Container className="container">
+                    <Row>
+                        <div className="col-lg-4 poster">
+                            <img src={IMG_PATH+peliculasDataLooped.poster} alt="Poster descriptivo" />
+                        </div>
+                        <div className="col-lg-8 movie-info">
+                            <div className="titulo-pelicula">
+                                <h1>{peliculasDataLooped.original_title}</h1><span>({peliculasDataLooped.release_date})</span>
+                                {/* <p>{peliculasDataLooped.genero}</p> */}
+                            </div>
+                            <p>{peliculasDataLooped.sinopsis}</p> 
+                            {/* <p>{peliculasDataLooped.director}</p> */}
+                            <p><b>Fecha de Lanzamiento</b> {peliculasDataLooped.release_date}</p>
+                        </div>
+                    </Row>   
+                </Container>
+            </div>
+            <Video url={peliculasDataLooped.video_url} subtitles={peliculasDataLooped.subtitles} />
+          </section>
+        }
+        
+      </>                   
     )
    
 }
-
 
 export default PeliculaDetalle;
