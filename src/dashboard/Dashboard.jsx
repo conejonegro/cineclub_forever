5d0a014be519958bc461072a5e43a186f4126cfa
Luis Rosales <lantoniorosales-contractor@dealerinspire.com>
2023-10-16 21:27:30 -0600

    s

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -108,7 +108,7 @@ function Dashboard(){
                 <form id="dashboardForm">
 
                     <div className="form-outline mb-4">
-                        <label>ID: </label>
+                        <label>The movie Datbase, movie ID: </label>
                         <input type="text" value={idValue} onChange={captureValue} className="form-control form-control-lg"/>
                     </div>
                     <div className="form-outline mb-4">
