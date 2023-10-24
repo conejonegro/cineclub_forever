e49335de719394b21787b20e409a969218311e7d
Luis Rosales <lantoniorosales-contractor@dealerinspire.com>
2023-10-24 16:19:37 -0600

    dashboard add id and homepage shows everything

--- src/components/TmdbAPICall.js
+++ src/components/TmdbAPICall.js
@@ -3,8 +3,12 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import PeliculaDetalle from '../pages/pelicula-detalle';
+import {dataFromFirebaseContext, ShowData} from '../dashboard/ShowData';
+import FirebaseSettings from "../components/FirebaseSettings";
+import { getFirestore } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore"; 
 
-
+const db = getFirestore(FirebaseSettings);
 const API_KEY = 'd35b24b361166e540ee6c082ddecd6bf';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/'
 const movies_id = [{ id: 460885  }, { id: 17111 },{ id: 772071 },{ id: 660942 },{ id: 9426 },{ id: 780609  }, { id: 882598}, {id: 7452}];
@@ -15,45 +19,86 @@ const movies_id = [{ id: 460885  }, { id: 17111 },{ id: 772071 },{ id: 660942 },
 //  const baseURL = `https://api.themoviedb.org/3/movie/346698/keywords?api_key=${API_KEY}`;
 // const baseURL = `https://api.themoviedb.org/3/search/movie?query=${nameQuery}&api_key=${API_KEY}`;
 
+
 function TmdbApiCall() {
+ 
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const location = useLocation();
   const isHomePage = location.pathname;
+  const [dataFromFirebase, setDataFromFirebase] = useState([]); // Usamos useState para gestionar el estado del arreglo.
 
-  useEffect(() => {
+    useEffect(() => {
 
-    const fetchData = async () => {
+      async function fetchDataFromFirebase() {
 
-      try {
-        const responses = await Promise.all(
-
-          movies_id.map((movie) =>
-            axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${API_KEY}&language=es-MX`)
-
-          )
-        );
-        
-        const postData = responses.map((response) => (
-          response.data
-          ));
-
-        setPosts(postData);
-        setLoading(false);
-         console.log(postData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false);
+        const data = [];
+        const querySnapshot = await getDocs(collection(db, 'peliculas'));
+        querySnapshot.forEach(doc => {
+          const daTaEach = doc.data();
+          if (daTaEach) {
+            data.push(daTaEach);
+          }
+        });
+  
+        //  console.log(data); // Esto imprimirá el arreglo en la consola.
+        setDataFromFirebase(data); // Actualizamos el estado con los datos obtenidos de Firebase.
+        console.log(data)
+       
       }
-    };
+  
+      
 
-    fetchData();
-  }, []);
+      const fetchData = async () => {
+
+       await fetchDataFromFirebase();
+      
+        try {
+          const responses = await Promise.all(
+  
+            arrayFromFirebase.map((movie) =>
+              axios.get(`https://api.themoviedb.org/3/movie/${movie.id}?api_key=${API_KEY}&language=es-MX`)
+  
+            )
+          );
+          console.log(responses)
+          
+          const postData = responses.map((response) => (
+            response.data
+            ));
+  
+          setPosts(postData);
+          setLoading(false);
+           console.log(postData);
+           console.log(posts);
+          
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        }
+      };
+  
+      fetchData();
+
+      
+
+    }, [loading]);
+
+    const arrayFromFirebase = dataFromFirebase.map(id => {
+     
+      return ({id: id.tmdb_id}) 
+    })
+
+      console.log(arrayFromFirebase)
+
+    
+      // console.log(movies_id)
+
 
   if (loading) {
     return <h5 className='container'>Cargando...</h5>;
   }
-  // console.log(posts)
+   console.log(posts)
 
 
   let myPosts = posts.map((post) => {
@@ -67,17 +112,20 @@ function TmdbApiCall() {
   // console.log(peliculasDataArraySliced)
 // Llamada a la API de Firebase
 
+if (!dataFromFirebase) {
+  return <div>Cargando datos de Firebase...</div>;
+}
 
 
   if (isHomePage === '/') {
     return(
       <div id="peliculasCont" className='container my-4'>
-       
+      
         <h1 className='my-4'>Ultimas peliculas...</h1>
-        <Row className="justify-content-between ">
+        <Row className="justify-content-center todas-peliculas-imdb">
           
-              {peliculasDataArraySliced.map((post) => {
-                return(
+              {peliculasDataArraySliced.map((post) => (
+               
                   <div className='movie-container' key={post.id}>
                     <Link to={"/peliculas-detalle/"+post.slug}>
                       <img src={IMG_PATH+post.poster} alt={post.nombre} className="poster " />
@@ -86,9 +134,9 @@ function TmdbApiCall() {
                       <p>{post.sinopsis.substring(0, 80)+'...'}</p>
                     </Link>
                 </div>
-                )
+               
                 
-              })}
+              ))}
 
         </Row>
         
@@ -99,7 +147,7 @@ function TmdbApiCall() {
     return (
       <div id="peliculasCont" className='container my-4'>
         <h1 className='my-4'>Todas peliculas.</h1>
-        <Row className="justify-content-between todas-peliculas-imdb">
+        <Row className="justify-content-center todas-peliculas-imdb">
           
               {myPosts.map((post) => {
                 return(
