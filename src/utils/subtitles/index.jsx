7b7c84f42a2089810baa57baf022a28c7e2a4bad
Luis Rosales <conejoconsal@hotmail.com>
2024-08-15 18:41:12 -0600

    super 8

--- src/utils/subtitles/index.jsx
+++ src/utils/subtitles/index.jsx
@@ -2,10 +2,22 @@ export const Subtitles = () => {
 
    const sutitlesObject = [
       {
+         tmdb_ID: "37686",
+         name: "super-8",
+         videoSrc: "https://mcseguros.com.mx/cineclub/super-8.mp4",
+         subtitlePath: "/static/subtitles/super-8-subs-esp.vtt"
+      },
+      {
          tmdb_ID: "42148",
          name: "pixote:-a-lei-do-mais-fraco",
          videoSrc: "https://mcseguros.com.mx/cineclub/pixote.mp4",
          subtitlePath: "/static/subtitles/pixote-esp-translated.vtt"
+      },
+      {
+         tmdb_ID: "533444",
+         name: "las-olas",
+         videoSrc: "https://mcseguros.com.mx/cineclub/wavves.mp4",
+         subtitlePath: "/static/subtitles/waves-new-trans.srt.vtt"
       },
       {
          tmdb_ID: "23655",
@@ -18,12 +30,6 @@ export const Subtitles = () => {
          name: "les-garçons-sauvages",
          videoSrc: "https://mcseguros.com.mx/cineclub/the-wild-boys.mp4",
          subtitlePath: "/static/subtitles/the-wild-boys-esp.vtt"
-      },
-      {
-         tmdb_ID: "533444",
-         name: "las-olas",
-         videoSrc: "https://mcseguros.com.mx/cineclub/wavves.mp4",
-         subtitlePath: "/static/subtitles/waves-new-trans.srt.vtt"
       },
 ]
 
