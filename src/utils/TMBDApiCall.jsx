63f976d404338870a2f86a9a76f176f84ff915d9
Luis Rosales <conejoconsal@hotmail.com>
2024-08-20 19:32:11 -0600

    si si

--- src/utils/TMBDApiCall.jsx
+++ src/utils/TMBDApiCall.jsx
@@ -1,25 +1,23 @@
 import axios from "axios";
 import { Subtitles } from "./subtitles";
 export default function TMDBApiCall() {
-
   const moviesData = Subtitles();
   const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-      const fetchData = async () => {
-        try {
-          const responses = await Promise.all(
-            moviesData.map((movie) =>
-              axios.get(`https://api.themoviedb.org/3/movie/${movie.tmdb_ID}?api_key=${API_KEY}&language=es-MX`)
-            )
-          );
-          const postData = responses.map((response) => (
-            response.data
-            ));
-            return postData;
-        } catch (error) {
-          console.error('Error fetching data:', error);
-         
-        }
-      };
-   return fetchData();
+  const fetchData = async () => {
+    try {
+      const responses = await Promise.all(
+        moviesData.map((movie) =>
+          axios.get(
+            `https://api.themoviedb.org/3/movie/${movie.tmdb_ID}?api_key=${API_KEY}&language=es-MX`
+          )
+        )
+      );
+      const postData = responses.map((response) => response.data);
+      return postData;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  return fetchData();
 }
---
+++ src/utils/ciclos/Teens.jsx
@@ -0,0 +1,10 @@
+export function Teens(){
+
+   const teensARRAY = [
+      {TMDB_ID: "37686"},
+      {TMDB_ID: "42148"},
+   ]
+
+   return teensARRAY
+
+}
